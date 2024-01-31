a256a89cfc23e6c1b05266f6a09c975278c4d6b6
YuMoua <153407360+YuMoua@users.noreply.github.com>
2024-01-30 19:20:32 -0600

    updated

--- assets/script.js
+++ assets/script.js
@@ -11,11 +11,11 @@
 
 
 $(function () {
-  
-//   let saveButtonEl = document.querySelector('saveBtn')
-//   saveButtonEl.addEventListener = ('click', function() {
-//     // need to add a save feature
-//   });
+
+  let saveButtonEl = $('saveBtn')
+  saveButtonEl.addEventListener = ('click', function() {
+    // need to add a save feature
+  });
 
 
   //
@@ -31,6 +31,10 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+
+
+
   //
   // TODO: Add code to display the current date in the header of the page.
 let currentDate = $('#currentDay');
@@ -42,5 +46,6 @@ let currentDate = $('#currentDay');
   }
 
 displayDate();
+setInterval(displayDate, 1000);
 
 })
